5ac1dc24f9de0558e85edbd1a7ef5438953e7dc3
hansani996 <hansanimadushika996@gamil.com>
2019-01-27 14:28:31 +0530

    inital commit

---
+++ src/main/java/lk/ijse/restController/CustomerCtrl.java
@@ -0,0 +1,25 @@
+package lk.ijse.restController;
+
+import lk.ijse.dto.CustomerDTO;
+import lk.ijse.entity.Customer;
+import lk.ijse.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("api/v1/customers")
+public class CustomerCtrl {
+
+    @Autowired
+    private CustomerService customerService;
+
+
+    @PostMapping
+    public void addCustomer(@RequestBody CustomerDTO customerDTO){
+        customerService.addCustomer(customerDTO);
+    }
+
+
+
+}
